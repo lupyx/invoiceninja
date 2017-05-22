ea3cbe3a38956571a828a870cd00646dc329ce75
lupyx <Noob159357456>
2017-05-22 14:43:53 +0200

    Created ClientReservationReceiver

---
+++ app/RabbitMQ/Receivers/ClientReservationReceiver.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: scrub
+ * Date: 22/05/17
+ * Time: 14:41
+ */
+
+namespace App\RabbitMQ\Receivers;
+
+
+use App\Models\Traits\SingleInstance;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class ClientReservationReceiver extends Receiver
+{
+    use SingleInstance;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->queue = 'new_HMS_order';
+    }
+
+    function onReceive(AMQPMessage $msg)
+    {
+       // $data = json_decode($msg->body);
+    }
+}
